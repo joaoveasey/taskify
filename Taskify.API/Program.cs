269324c4e6ec532c45d74266d95fd5622520417b
joaoveasey <jovepedro@gmail.com>
2024-09-12 20:05:07 -0300

    feat: configurando identity no db context e configurando o swagger

--- Taskify.API/Program.cs
+++ Taskify.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using Taskify.API.Infra;
 using Taskify.API.Interfaces;
@@ -51,10 +52,36 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(connection, serverVersion);
 });
 
-// configurando annotations nos endpoints do swagger
+// configurando o swagger
 builder.Services.AddSwaggerGen(c =>
 {
-    c.EnableAnnotations();
+    c.EnableAnnotations(); // configurando annotations nos endpoints do swagger
+    c.SwaggerDoc("v1", new() { Title = "TaskifyAPI", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme // adiciona a definição de segurança para o swagger como Bearer
+    {
+        Description = "Bearer JWT",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string [] { }
+        }
+    });
 });
 
 var app = builder.Build();

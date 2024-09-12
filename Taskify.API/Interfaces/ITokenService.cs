765a5933cdbf57bdac74f028412857f1b59c8f1a
joaoveasey <jovepedro@gmail.com>
2024-09-12 19:50:45 -0300

    feat: implementando serviço de criação do token JWT

---
+++ Taskify.API/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Taskify.API.Interfaces
+{
+    public interface ITokenService
+    {
+        JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration configuration);
+    }
+}
--- Taskify.API/Program.cs
+++ Taskify.API/Program.cs
@@ -1,20 +1,47 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using Taskify.API.Infra;
 using Taskify.API.Interfaces;
 using Taskify.API.Repository;
+using Taskify.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var secretKey = builder.Configuration["JWT:SecretKey"] 
+    ?? throw new ArgumentException("Chave JWT inválida.");
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
 
 // injeção de dependencia
 builder.Services.AddScoped<ITasksRepository, TasksRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+// middleware de autenticação JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // define o esquema de autenticação padrão como JWT
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => { // adiciona a configuração do middleware de autenticação JWT
+    options.SaveToken = true; 
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ClockSkew = TimeSpan.Zero, // remove a margem de erro do token
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+    };
+});
 
 // contexto do banco de dados
 builder.Services.AddDbContext<ApplicationDbContext>(options =>

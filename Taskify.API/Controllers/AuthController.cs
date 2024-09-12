6b56504289ca725bf4a1128dd6342c107d3990f9
joaoveasey <jovepedro@gmail.com>
2024-09-12 20:14:32 -0300

    feat: implementando endpoint de login com jwt

---
+++ Taskify.API/Controllers/AuthController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Taskify.API.Interfaces;
+using Taskify.API.Models;
+using Taskify.API.Models.DTO;
+
+namespace Taskify.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _signInManager;
+        private readonly IConfiguration _config;
+
+        public AuthController(ITokenService tokenService, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> signInManager, IConfiguration config)
+        {
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _config = config;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModelDTO model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username!);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password!))
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName!),
+                    new Claim(ClaimTypes.Email, user.Email!),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                foreach (var role in roles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
+
+                var token = _tokenService.GenerateAccessToken(authClaims, _config);
+
+                await _userManager.UpdateAsync(user);
+
+                return Ok(new
+                {
+                    Token = new JwtSecurityTokenHandler().WriteToken(token),
+                    Expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+
+    }
+}

6d450de91ca0195e722047e2c2bbf40aa23afe74
joaoveasey <jovepedro@gmail.com>
2024-09-14 16:40:22 -0300

    feat: adicionando camada de testes para TaskController e AuthController (ainda sem funcionalidade)

---
+++ Taskify.API.Test/TasksUnitTestController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taskify.API.Infra;
+using Taskify.API.Interfaces;
+using Taskify.API.Repository;
+
+namespace Taskify.API.Test
+{
+    public class TasksUnitTestController
+    {
+        public IUnitOfWork repository;
+        public static DbContextOptions<ApplicationDbContext> dbContextOptions;
+
+        public static string connectionString =
+            "Server=127.0.0.1;Port=3306;User ID=root;Password=jvp_04022005;Database=taskify_db";
+
+        static TasksUnitTestController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                .Options;
+        }
+
+        public TasksUnitTestController()
+        {
+            var context = new ApplicationDbContext(dbContextOptions);
+            repository = new UnitOfWork(context);
+        }
+    }
+}
---
+++ Taskify.API.Test/UnitTests/Auth/LoginUnitTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taskify.API.Test.UnitTests.Auth
+{
+    internal class LoginUnitTest
+    {
+    }
+}
